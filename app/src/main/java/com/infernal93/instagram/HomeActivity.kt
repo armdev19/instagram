614fcabde3a302324f3cfa6683ca76f2a3489d0e
armdev19 <armdev19@gmail.com>
2019-10-01 17:34:52 +0400

    - Add LoginActivity - First design for LoginActivity

--- app/src/main/java/com/infernal93/instagram/HomeActivity.kt
+++ app/src/main/java/com/infernal93/instagram/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.infernal93.instagram
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
@@ -7,19 +8,31 @@ import com.google.firebase.auth.FirebaseAuth
 class HomeActivity : BaseActivity(navNumber = 0) {
     private val TAG = "HomeActivity"
 
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         Log.d(TAG, "onCreate")
         setupBottomNav()
 
-        val auth = FirebaseAuth.getInstance()
-        auth.signInWithEmailAndPassword("armdev19@gmail.com", "123456")
-            .addOnCompleteListener {
-                if(it.isSuccessful) {
-                    Log.d(TAG, "signIn: success")
-                } else Log.e(TAG, "signIn: failure", it.exception)
-            }
 
+        mAuth = FirebaseAuth.getInstance()
+        mAuth.signOut()
+       // mAuth.signInWithEmailAndPassword("armdev19@gmail.com", "123456")
+         //   .addOnCompleteListener {
+           //     if(it.isSuccessful) {
+             //       Log.d(TAG, "signIn: success")
+               // } else Log.e(TAG, "signIn: failure", it.exception)
+           // }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (mAuth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
