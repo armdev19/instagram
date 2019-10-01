bb908d9a80c73d6b9bcc96c68ac27fe018814baf
armdev19 <armdev19@gmail.com>
2019-10-01 05:34:47 +0400

    - Add FireBase authentication

--- app/src/main/java/com/infernal93/instagram/HomeActivity.kt
+++ app/src/main/java/com/infernal93/instagram/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.infernal93.instagram
 
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : BaseActivity(navNumber = 0) {
     private val TAG = "HomeActivity"
@@ -10,8 +11,15 @@ class HomeActivity : BaseActivity(navNumber = 0) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         Log.d(TAG, "onCreate")
-
         setupBottomNav()
+
+        val auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword("armdev19@gmail.com", "123456")
+            .addOnCompleteListener {
+                if(it.isSuccessful) {
+                    Log.d(TAG, "signIn: success")
+                } else Log.e(TAG, "signIn: failure", it.exception)
+            }
 
     }
 }
